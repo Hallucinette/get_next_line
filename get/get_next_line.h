7de1790175a9022556509a6e53ef1ae8dd68b105
Amélie POCCHIOLO <amelie@MacBook-Pro-de-Amelie.local>
2020-02-18 20:00:48 +0100

    commit

---
+++ get/get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amepocch <amepocch@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/13 22:09:58 by amepocch          #+#    #+#             */
+/*   Updated: 2020/02/13 22:24:37 by amepocch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+
+#  define BUFFER_SIZE 32
+
+# endif
+
+# include <unistd.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+size_t	ft_strlen(const char *s);
+int		get_next_line(const int fd, char **line);
+char	*ft_fstrjoin(char *s1, char const *s2);
+int		ft_instrchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
---
+++ get/get_next_line_utils.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amepocch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/13 22:07:53 by amepocch          #+#    #+#             */
+/*   Updated: 2020/02/13 22:09:34 by amepocch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t		ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static char	*ft_strcat(char *str1, char *str2)
+{
+	int		len;
+	int		i;
+
+	len = ft_strlen(str1);
+	i = 0;
+	while (str2[i])
+	{
+		str1[len + i] = str2[i];
+		i++;
+	}
+	str1[len + i] = '\0';
+	return (str1);
+}
+
+char		*ft_fstrjoin(char *s1, char const *s2)
+{
+	int		i;
+	int		total_size;
+	char	*new;
+
+	if (!s1 || !s2)
+		return (NULL);
+	total_size = ft_strlen(s1) + ft_strlen(s2);
+	i = 0;
+	if (!(new = malloc(total_size + 1)))
+		return (NULL);
+	new[0] = '\0';
+	new = ft_strcat(new, (char *)s1);
+	new = ft_strcat(new, (char *)s2);
+	free((char *)s1);
+	return (new);
+}
+
+int			ft_instrchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+
+	str = (char*)s;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char		*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	len_s;
+	char	*dst;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	len_s = ft_strlen(s);
+	if (!(dst = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	dst[0] = '\0';
+	if (len_s < start)
+		return (dst);
+	if (start + len > len_s)
+		len = len_s - start;
+	while (i < len)
+	{
+		dst[i] = s[start];
+		i++;
+		start++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
